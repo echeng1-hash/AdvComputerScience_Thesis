c8ef23867fb07eff6002b28f021f183a67b51532
echeng1-hash <echeng1@hwemail.com>
2025-09-08 14:57:22 -0700

    9/8
    toString & equals

--- Day0/PaintingTester.java
+++ Day0/PaintingTester.java
@@ -6,5 +6,14 @@ public class PaintingTester {
         sevenBirds.broke();
         sevenBirds.viewed();
         System.out.println(sevenBirds.toString());
+        
+        Painting numberAPaint = new Painting();
+        Painting numberBPaint = new Painting();
+
+        if (numberAPaint.equals(numberBPaint)) {
+            System.out.println("They are equal.");
+        } else {
+            System.out.println("They are not equal.");
+        }
     }
 }
