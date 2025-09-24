d5120e5433380e21c2450aad4194ceb3617584a6
echeng1-hash <echeng1@hwemail.com>
2025-09-24 15:27:29 -0700

    9/24

--- Day0/Line.java
+++ Day0/Line.java
@@ -9,6 +9,13 @@ public class Line {
         this.b = b;
         this.c = c;
     }
+
+    // default constructor
+    public Line() {
+        this.a = generateRandomParameters();
+        this.b = generateRandomParameters();
+        this.c =  (int) ((Math.random()) * 41) - 20;
+    }
     
     public int getA() {
         return a;
@@ -46,23 +53,42 @@ public class Line {
     }
     
     public String toString() {
-        system.out.printLn ("Equation: " + a + "x" + "+" 
-                            + b + "y" + c + " = 0");
+        return "Equation: " + a + "x" + "+" 
+                + b + "y+" + c + " = 0";
     }
     
-    public boolean equals(Line other){
-        
+    public boolean equals(Line other) { // Compares ALL instance variables for equality
+        if (this.a == other.a && this.b == other.b && this.c == other.c) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
     public int generateRandomParameters() {
-        
+        // -20 to 20
+        int x = (int) ((Math.random()) * 41) - 20;
+        if (x >= 0) {
+            x += 1;
+        }
+        return x;
     }
     
-    public boolean isParallel(int a2, int b2, int c2) {
-        
+    public boolean isParallel(Line original, Line other) {
+        // cross-multiplyz: a1/b1 == a2/b2  ->  a1*b2 == a2*b1
+        return a * other.b == other.a * b;
     }
     
-    public double intersectionPoint(int a2, int b2, int c2) {
-        
+    public String intersectionPoint(Line original, Line other) {
+        if (isParallel(original, other)) {
+            return "No intersection points.";
+        } else { // isolate "y" to get "x", then plug "x" back in the equation to get "y"
+            int det = a * other.b - other.a * b; // denominator
+            int numOfX = b * other.c - other.b * c; // numerators
+            int numOfY = other.a * c - a * other.c;
+            double x = (double) numOfX / det; // divide as double
+            double y = (double) numOfY / det;
+            return "(" + x + "," + y + ")";
+        }
     }
 }
--- Day0/LineTester.java
+++ Day0/LineTester.java
@@ -1,5 +1,63 @@
 public class LineTester {
     public static void main(String[] args) {
+        // Testing Line 1
+        Line line1 = new Line(5, 4, -17);
+        double slope1 = line1.calculateSlope(); // slope1 should be -1.25
+        System.out.println(slope1);
+
+        // Should be true, as 5(5) + 4(-2) + (-17) = 0
+        boolean onLine1 = line1.isCoordinateOnLine(5, -2);
+        System.out.println(onLine1);
+
+        // Testing Line 2
+        Line line2 = new Line(-25, 40, 30);
+        double slope2 = line2.calculateSlope(); // slope2 should be 0.625
+        System.out.println(slope2);
+
+        // Should be false as -25(5) + 40(-2) + 30 != 0
+        boolean onLine2 = line2.isCoordinateOnLine(5, -2);
+        System.out.println(onLine2);
+
+        // Testing getA() and setA(). Should print 5, and then 2
+        int line1A = line1.getA();
+        System.out.println(line1A);
+        line1.setA(2);
+        line1A = line1.getA();
+        System.out.println(line1A);
+
+        // Testing getB() and setB(). Should print 4, and then 43
+        int line1B = line1.getB();
+        System.out.println(line1B);
+        line1.setB(43);
+        line1B = line1.getB();
+        System.out.println(line1B);
+
+        // Testing getC() and setC(). Should print -17, and then -14
+        int line1C = line1.getC();
+        System.out.println(line1C);
+        line1.setC(-14);
+        line1C = line1.getC();
+        System.out.println(line1C);
+
+        // Testing isParallel(Line original, Line other).
+        // should say not parallel
+        System.out.println(line1.isParallel(line1, line2));
+        // should say parallel
+        Line line3 = new Line(2, 43, 17);
+        System.out.println(line3.isParallel(line3, line1));
         
+        // Testing equals(Line other).
+        // should be not equal
+        System.out.println(line3.equals(line1));
+        // should be equal
+        Line line4 = new Line(2, 43, -14);
+        System.out.println(line1.equals(line4));
+
+        // Testing intersectionPoint(Line original, Line other).
+        System.out.println(line2.intersectionPoint(line2, line3));
+        
+        // Testing Line(), which also confirms generateRandomParameters().
+        Line defaultLine = new Line();
+        System.out.println(defaultLine);
     }
 }
